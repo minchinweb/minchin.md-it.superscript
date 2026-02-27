eb0110a12ae4de8aeefa9adababe4a0ce30bfea1
MinchinWeb <w_minchin@hotmail.com>
2026-02-26 20:45:12 -0700

    [Release] 1.0.0
    c.f. https://github.com/executablebooks/mdit-py-plugins/pull/128
    Fixes https://github.com/minchinweb/minchin.pelican.readers.commonmark/issues/6

--- minchin/md_it/superscript/__init__.py
+++ minchin/md_it/superscript/__init__.py
@@ -6,7 +6,7 @@ __all__ = ("superscript_plugin",)
 
 __title__ = "minchin.md-it.superscript"
 __tagline__ = "Superscript, for Markdown-IT-Py"
-__version__ = "1.0.0-dev"
+__version__ = "1.0.0"
 __author__ = "William Minchin"
 __email__ = "w_minchin@hotmail.com"
 __license__ = "MIT License"
