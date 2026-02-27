605651c8140a5e1baae8ad3d0991f9e5cf4ac811
MinchinWeb <w_minchin@hotmail.com>
2026-02-26 20:30:02 -0700

    Add standalone project scaffolding

---
+++ CHANGELOG.rst
@@ -0,0 +1,10 @@
+Changelot for Superscipt
+========================
+
+A plugin for Markdown-IT-py
+
+- :feature:`-` initial port, from a `Pull Request
+  <https://github.com/executablebooks/mdit-py-plugins/pull/128>`_ against the
+  ``mdit-py-plugins`` repo by @elijahgreenstein.  See also my CommonMark reader
+  for Pelican, `Issue 6
+  <https://github.com/minchinweb/minchin.pelican.readers.commonmark/issues/6>`_.
--- minchin/md_it/superscript/__init__.py
+++ minchin/md_it/superscript/__init__.py
@@ -3,3 +3,12 @@
 from .index import superscript_plugin
 
 __all__ = ("superscript_plugin",)
+
+__title__ = "minchin.md-it.superscript"
+__tagline__ = "Superscript, for Markdown-IT-Py"
+__version__ = "1.0.0-dev"
+__author__ = "William Minchin"
+__email__ = "w_minchin@hotmail.com"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2026 William Minchin"
+__url__ = "https://github.com/MinchinWeb/minchin.md-it.superscript"
